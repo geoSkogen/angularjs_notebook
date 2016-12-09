a5b8c9eeb695edabaaabe8bba1b8dbd311ca43eb
joseph_scoggins <borninthecaul@gmail.com>
2016-12-09 12:08:32 -0800

    new lessons

--- thisApp.js
+++ thisApp.js
@@ -1,8 +1,32 @@
 //create a object of the module FIND OUT WHAT'S UP WITH THIS EMPTY ARRAY
 var app = angular.module('thisApp', []);
+
 var app2 = angular.module("thisApp2", []);
+app2.filter('customFilter', function () {
+  return function (x) {
+    var c, txt = "";
+    for (var i = 0; i < x.length; i++) {
+      c = x[i];
+      if (i % 2 == 0) {
+        c = c.toUpperCase();
+      }
+      txt += c;
+    }
+    return txt;
+  };
+});
+app2.service('hexafy', function () {
+  this.toHex = function (x) {
+    return x.toString(16);
+  }
+});
+app2.filter('hexFormat', ['hexafy', function(hexafy) {
+  return function (x) {
+    return hexafy.toHex(x);
+  };
+}]);
 
 var appColor = angular.module("appColor", []);
 appColor.run(function ($rootScope) {
   $rootScope.color = "blue";
-})
+});
--- thisControlHash.js
+++ thisControlHash.js
@@ -1,13 +1,48 @@
-app2.controller("thisControlHash", function ($scope) {
+app2.controller("thisControlHash", function ($scope, $location,  $timeout, $interval, hexafy) {
   //place hashes into an array
   $scope.names = [
     {lname: 'Redjyk', fname: 'Magda', dept: 'Produce'},
     {lname: 'Glazier', fname: 'Stephanie', dept: 'Beer & Wine'},
-    {lname: 'Ricker', fname: 'Chase', dept: 'Prepared Foods'}
+    {lname: 'Ricker', fname: 'Chase', dept: 'Prepared Foods'},
+    {lname: 'Garcia', fname: 'Marc', dept: 'Bakery'},
   ];
   $scope.prices = [
     {day: 60, month: 750, year: 7000},
     {day: 10, month: 250, year: 2000},
     {day: 1200, month: 27000, year: 300000}
   ];
+  $scope.namesKit = [
+    "Maria",
+    "Julia",
+    "Justina",
+    "Leticia",
+    "Elia",
+    "Ana",
+    "Orbelina"
+  ];
+  //this object assignment breaks script and I don't know why.
+  /*$scope.albums = {
+    Upsilon Acrux: "Radian Futura",
+    Secret Chiefs III: "Book of Horizons",
+    Kayo Dot: "Choirs of the Eye",
+    Sleepytime Gorilla Museum: "In Glorious Times",
+    Animals as Leaders: "Weightless"
+  };*/
+  $scope.changeOrder = function (x) {
+    $scope.selectOrder = x;
+  };
+  $scope.thisUrl = $location.absUrl();
+
+  $scope.thisHeader = "this is a header";
+  $timeout(function () {
+    $scope.thisHeader = "this a new header";
+  }, 2000);
+
+  $scope.thisTime = new Date().toLocaleTimeString();
+  $interval(function () {
+  $scope.thisTime = new Date().toLocaleTimeString();
+  }, 1000);
+
+//  $scope.hex = hexafy.toHex(Number(225));
+
 });
