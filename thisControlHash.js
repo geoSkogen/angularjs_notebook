ab9e68aa349e53812f57d5d1e7cb2017a62e597b
joseph_scoggins <borninthecaul@gmail.com>
2016-12-14 20:42:51 -0800

    DOM and form stuff

--- thisControlHash.js
+++ thisControlHash.js
@@ -1,4 +1,10 @@
 app2.controller("thisControlHash", function ($scope, $location,  $timeout, $interval, hexafy) {
+  $scope.mouseovers = 0;
+  $scope.mousemoves = 0;
+  $scope.togState = false;
+  $scope.togName = "show"
+  $scope.tog = 0;
+  $scope.test = "_X_";
   //place hashes into an array
   $scope.names = [
     {lname: 'Redjyk', fname: 'Magda', dept: 'Produce'},
@@ -20,7 +26,7 @@ app2.controller("thisControlHash", function ($scope, $location,  $timeout, $inte
     "Ana",
     "Orbelina"
   ];
-  //this object assignment breaks script and I don't know why.
+  //this object assignment breaks the script and I don't know why.
   /*$scope.albums = {
     Upsilon Acrux: "Radian Futura",
     Secret Chiefs III: "Book of Horizons",
@@ -28,6 +34,21 @@ app2.controller("thisControlHash", function ($scope, $location,  $timeout, $inte
     Sleepytime Gorilla Museum: "In Glorious Times",
     Animals as Leaders: "Weightless"
   };*/
+$scope.master = { firstName: "enter first name", lastName: "enter last name"};
+$scope.reset = function () {
+  $scope.user = angular.copy($scope.master);
+};
+  $scope.showToggle = function () {
+    if ($scope.tog == 0) {
+      $scope.togName = "hide";
+      $scope.togState = true;
+      $scope.tog = 1;
+    } else {
+      $scope.togName = "show";
+      $scope.togState = false;
+      $scope.tog = 0;
+    }
+  }
   $scope.changeOrder = function (x) {
     $scope.selectOrder = x;
   };
@@ -44,5 +65,5 @@ app2.controller("thisControlHash", function ($scope, $location,  $timeout, $inte
   }, 1000);
 
 //  $scope.hex = hexafy.toHex(Number(225));
-
+$scope.reset();
 });
