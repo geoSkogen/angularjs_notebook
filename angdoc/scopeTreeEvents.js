c770610e641428a061ebe0336066e9d1ba1d0df5
joseph_scoggins <borninthecaul@gmail.com>
2016-12-27 20:53:48 -0800

    fixes scope_tree_events app

--- angdoc/scopeTreeEvents.js
+++ angdoc/scopeTreeEvents.js
@@ -2,7 +2,7 @@ var scopeTreeEvents = angular.module("scopeTreeEvents", []);
 
 scopeTreeEvents.controller('eventController', ['$scope', function ($scope) {
   $scope.count = 0;
-  $scope.$on('anEvent'), function () {
+  $scope.$on('anEvent', function () {
     $scope.count++;
-  };
+  });
 }]);
